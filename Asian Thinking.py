e197b2fba6799926a04bb4ea0dd779023c5edf86
kxha <33325640+kxha@users.noreply.github.com>
2017-11-03 11:47:11 -0400

    Add files via upload

---
+++ Asian Thinking.py
@@ -0,0 +1,275 @@
+import turtle
+
+bob=turtle.Turtle()
+turtle.bgcolor("black")
+bob.speed(20)
+
+bob.color("red")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("green")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+        
+         
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("orange")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("blue")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("white")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("purple")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("pink")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+
+
+
+bob.color("skyblue")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+
+
+bob.color("yellow")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("red")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("green")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+        
+         
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("orange")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("blue")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("white")
+
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("purple")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+bob.color("pink")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+
+
+
+bob.color("skyblue")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+
+
+bob.color("yellow")
+for times in range(10):
+    for times in range(2):
+        bob.forward(200)
+        bob.right(60)
+        bob.forward(120)
+        bob.right(36)
+
+
+bob.penup()
+bob.right(90)#try different angles to move the designs around the screen
+bob.forward(20)
+bob.pendown()
+
+
+
+turtle.done()
